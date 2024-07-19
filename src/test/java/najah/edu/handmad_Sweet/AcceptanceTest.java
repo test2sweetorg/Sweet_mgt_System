7593e189e149e980fb9eff53b82b80a3f7713b72
malik ali <angrymalik197@gmail.com>
2024-07-19 18:04:23 +0300

    add the use_case folder

---
+++ src/test/java/najah/edu/handmad_Sweet/AcceptanceTest.java
@@ -0,0 +1,23 @@
+package najah.edu.handmad_Sweet;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features="use_cases",
+		plugin = {"summary","html:target/cucumber/report.html"},
+		monochrome=true,
+		snippets = SnippetType.CAMELCASE,
+		glue ="najah.edu.handmad_Sweet"
+		)
+
+
+
+
+public class AcceptanceTest {
+
+}
